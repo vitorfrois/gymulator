e1bbd79958742f4ae054d4c01a6393564871e15d
Vítor Fróis <frois@usp.br>
2022-12-19 18:30:46 -0300

    Fixed some errors
    added try/except to update functions

--- gym.py
+++ gym.py
@@ -26,6 +26,15 @@ def generate_bar(person='Pessoa', reps=5) -> Progress:
     job_progress.add_task(f"{person} treinando... ", total=reps, visible=True)
     return job_progress
 
+def recreate_table(available_machines, n_machines) -> Progress:
+    for task in table.tasks:
+        table.remove_task(task)
+    for elem in available_machines:
+        table.add_task(elem + " " + 
+            str(n_machines[elem]) + "/" + 
+            str(available_machines[elem]))
+    return table
+
 console = Table().grid()
 
 progress_table = Table(expand=False).grid()
@@ -35,33 +44,11 @@ progress_table.add_row(
 )
 progress_table.add_row(
     Panel.fit(job_progress, width=50, title="[b]Jobs", border_style="red"),
-    Panel.fit(console, width=80, title="[b]Log", border_style="red")
+    # Panel.fit(console, width=80, title="[b]Log", border_style="red")
 )
-# progress_table.add_row(
-# )
-
-# def generate_table():
-#     """Make a new table."""
-#     table = Table()
-#     table.add_column("ID")
-#     table.add_column("Value")
-#     table.add_column("Status")
-
-#     for row in range(random.randint(2, 6)):
-#         value = random.random() * 100
-#         table.add_row(
-#             f"{row}", f"{value:3.2f}", "[red]ERROR" if value < 50 else "[green]SUCCESS"
-#         )
-#     return table
 
 live = Live(progress_table, refresh_per_second=10)
 
-def recreate_table(available_machines, n_machines):
-    for task in table.tasks:
-        table.remove_task(task)
-    for elem in available_machines:
-        table.add_task(elem + " " + str(n_machines[elem]) + "/ total")
-               
 
 def init_live():
     with live:
@@ -129,17 +116,22 @@ class Gym:
         console.add_row(f"{person_name} will be doing {reps} reps of {display_name}")
         console.add_row(f"{semaphore._value}/{n_machines} available.")
         
-        table.update(recreate_table(self.available_machines, self.n_machines))
-
+        
         try:
             job_progress.update(generate_bar(person_name, reps))
+            table.update(recreate_table(self.available_machines, self.n_machines))
         except:
             pass
+
         for _ in range(reps):
-            time.sleep(1)
+            time.sleep(0.2)
+
         semaphore.release()
 
-        table.update(recreate_table(self.available_machines, self.n_machines))
+        try:
+            table.update(recreate_table(self.available_machines, self.n_machines))
+        except:
+            pass
 
         # print(f"{person_name} has finished using {display_name}", end='\t\t')
         # print(f"{semaphore._value}/{n_machines} available.")
